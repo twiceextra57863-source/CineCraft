0f23ce0d7aa778af6d8e3ded637083429e8660bf
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-07 00:59:38 +0530

    Update PostProcessing.java

---
+++ src/main/java/com/yourname/cinecraft/rendering/PostProcessing.java
@@ -0,0 +1,14 @@
+package com.yourname.cinecraft.rendering;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Applies post-processing effects
+ */
+public class PostProcessing {
+
+    public BufferedImage applyEffects(BufferedImage input) {
+        // TODO: Depth of Field, Motion Blur, Color Grading
+        return input;
+    }
+}
