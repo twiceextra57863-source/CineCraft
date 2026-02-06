b0121693f0427774adc7bcfd64192ffa6475b654
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-07 00:59:10 +0530

    Update LightManager.java

---
+++ src/main/java/com/yourname/cinecraft/rendering/LightManager.java
@@ -0,0 +1,17 @@
+package com.yourname.cinecraft.rendering;
+
+import java.awt.Color;
+
+/**
+ * Handles scene lights
+ */
+public class LightManager {
+
+    public void applyLighting() {
+        // TODO: Implement point/directional lights
+    }
+
+    public Color getAmbientLight() {
+        return Color.WHITE;
+    }
+}
