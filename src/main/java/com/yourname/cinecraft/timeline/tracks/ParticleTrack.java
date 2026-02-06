225ef175db0b08122df09caa5117720a492adbb9
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-07 00:51:52 +0530

    Update ParticleTrack.java

---
+++ src/main/java/com/yourname/cinecraft/timeline/tracks/ParticleTrack.java
@@ -0,0 +1,13 @@
+package com.yourname.cinecraft.timeline.tracks;
+
+import com.yourname.cinecraft.timeline.Track;
+
+public class ParticleTrack extends Track {
+
+    public ParticleTrack(String name) { super(name); }
+
+    @Override
+    public void applyFrame(int frame) {
+        // TODO: Trigger particle events
+    }
+}
