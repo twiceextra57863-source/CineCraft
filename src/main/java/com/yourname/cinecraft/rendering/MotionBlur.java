116ecd27378aa8be8738b2df65df45ec34e4adb0
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-07 01:00:11 +0530

    Update MotionBlur.java

---
+++ src/main/java/com/yourname/cinecraft/rendering/MotionBlur.java
@@ -0,0 +1,14 @@
+package com.yourname.cinecraft.rendering;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Motion blur effect
+ */
+public class MotionBlur {
+
+    public BufferedImage apply(BufferedImage input) {
+        // TODO: implement motion blur
+        return input;
+    }
+}
