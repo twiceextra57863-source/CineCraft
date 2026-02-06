c9999ee76341998b8d7b244d39c8da2dff728f24
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-07 00:51:37 +0530

    Update AudioTrack.java

---
+++ src/main/java/com/yourname/cinecraft/timeline/tracks/AudioTrack.java
@@ -0,0 +1,13 @@
+package com.yourname.cinecraft.timeline.tracks;
+
+import com.yourname.cinecraft.timeline.Track;
+
+public class AudioTrack extends Track {
+
+    public AudioTrack(String name) { super(name); }
+
+    @Override
+    public void applyFrame(int frame) {
+        // TODO: Play audio events
+    }
+}
