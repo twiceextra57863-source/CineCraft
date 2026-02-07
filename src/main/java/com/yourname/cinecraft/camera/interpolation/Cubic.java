8f1016d32514008ea0c367ef419bfbbca4c5ac41
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-07 07:05:57 +0530

    Update Cubic.java

--- src/main/java/com/yourname/cinecraft/camera/interpolation/Cubic.java
+++ src/main/java/com/yourname/cinecraft/camera/interpolation/Cubic.java
@@ -1,14 +1,9 @@
 package com.yourname.cinecraft.camera.interpolation;
 
-public class Cubic {
-
-    public static double interpolate(double a, double b, double t) {
-        t = t*t*t; // cubic easing
-        return a + (b - a) * t;
-    }
-
-    public static float interpolate(float a, float b, float t) {
-        t = t*t*t;
-        return a + (b - a) * t;
+public class Cubic implements Interpolation {
+    @Override
+    public double interpolate(double start, double end, double alpha) {
+        double t = alpha*alpha*alpha;
+        return start + (end - start) * t;
     }
 }
