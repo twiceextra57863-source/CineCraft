365b0e3e561d9e8363d5cd69a6ec19dec35f139d
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-07 07:05:38 +0530

    Update EaseInOut.java

--- src/main/java/com/yourname/cinecraft/camera/interpolation/EaseInOut.java
+++ src/main/java/com/yourname/cinecraft/camera/interpolation/EaseInOut.java
@@ -1,14 +1,9 @@
 package com.yourname.cinecraft.camera.interpolation;
 
-public class EaseInOut {
-
-    public static double interpolate(double a, double b, double t) {
-        t = t * t * (3 - 2 * t); // smoothstep
-        return a + (b - a) * t;
-    }
-
-    public static float interpolate(float a, float b, float t) {
-        t = t * t * (3 - 2 * t);
-        return a + (b - a) * t;
+public class EaseInOut implements Interpolation {
+    @Override
+    public double interpolate(double start, double end, double alpha) {
+        double t = alpha*alpha*(3 - 2*alpha);
+        return start + (end - start) * t;
     }
 }
