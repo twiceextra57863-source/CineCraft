8dfde2a13de3889c85531573d2b1a6fa2c771bad
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-07 06:53:41 +0530

    Update DepthOfField.java

--- src/main/java/com/yourname/cinecraft/rendering/DepthOfField.java
+++ src/main/java/com/yourname/cinecraft/rendering/DepthOfField.java
@@ -1,14 +1,5 @@
-package com.yourname.cinecraft.rendering;
-
-import java.awt.image.BufferedImage;
-
-/**
- * Depth of Field effect
- */
-public class DepthOfField {
-
-    public BufferedImage apply(BufferedImage input) {
-        // TODO: implement blur based on depth
-        return input;
-    }
+// DepthOfField.java
+public BufferedImage apply(BufferedImage input, int focusDistance){
+    // TODO: Blur pixels far from focusDistance
+    return input;
 }
