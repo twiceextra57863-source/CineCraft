6e94858382d0071b99f71176b733c5e9c25e39d8
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-07 07:02:52 +0530

    Update Keyframe.java

--- src/main/java/com/yourname/cinecraft/animation/Keyframe.java
+++ src/main/java/com/yourname/cinecraft/animation/Keyframe.java
@@ -1,13 +1,17 @@
 package com.yourname.cinecraft.animation;
 
+/**
+ * Stores actor position/rotation at a tick
+ */
 public class Keyframe {
-    public final int frame;
-    public final float[] rotation; // pitch,yaw,roll
-    public final double[] position; // x,y,z
 
-    public Keyframe(int frame, double x, double y, double z, float pitch, float yaw, float roll){
-        this.frame = frame;
-        this.position = new double[]{x,y,z};
-        this.rotation = new float[]{pitch,yaw,roll};
+    public int tick;
+    public double x, y, z;
+    public double yaw, pitch;
+
+    public Keyframe(int tick, double x, double y, double z, double yaw, double pitch){
+        this.tick = tick;
+        this.x = x; this.y = y; this.z = z;
+        this.yaw = yaw; this.pitch = pitch;
     }
 }
